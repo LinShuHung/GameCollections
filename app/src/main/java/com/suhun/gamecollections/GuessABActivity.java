3fe9207227665336afedefcf588a4d43b8bf0be3
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-04-11 11:07:35 +0800

    Add button behavior for enter guessAB game and exit game

--- app/src/main/java/com/suhun/gamecollections/GuessABActivity.java
+++ app/src/main/java/com/suhun/gamecollections/GuessABActivity.java
@@ -3,6 +3,7 @@ package com.suhun.gamecollections;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.suhun.gamecollections.databinding.ActivityGuessAbactivityBinding;
 
@@ -14,5 +15,11 @@ public class GuessABActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityGuessAbactivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/suhun/gamecollections/MainActivity.java
+++ app/src/main/java/com/suhun/gamecollections/MainActivity.java
@@ -1,19 +1,42 @@
 package com.suhun.gamecollections;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.suhun.gamecollections.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    String tag = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
+    private ActivityResultLauncher startBackActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.d(tag, "----Go Back Here----");
+
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.GuessAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GuessABActivity.class);
+                startBackActivity.launch(intent);
+            }
+        });
     }
 
 }
